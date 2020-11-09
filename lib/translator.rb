3b4da1078e4488d9739d51461121afa89cccef49
Charlie Knight <williamchknight@gmail.com>
2020-11-09 05:14:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -36,7 +36,7 @@ def get_english_meaning(path, emoticon)
   invert.each do |emos, eng|
     english << eng
     emos.each do |items|
-      emotes << items
+      emotes << items.each
     end 
   end    
   binding.pry
