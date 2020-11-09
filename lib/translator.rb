371c7f55021cf9326b306d61128098a488520e03
Charlie Knight <williamchknight@gmail.com>
2020-11-09 04:06:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,11 +17,8 @@ def get_japanese_emoticon(path, emoticon)
   emoticon_hash.each do |word, symbols|
    symbols.each do |k, v|
      empty << v  
-     empty = empty.each_slice(2).map do |a,b|
-       [a, b].to_h 
-     end 
    end 
-  end
+  end 
   binding.pry 
 end
 
