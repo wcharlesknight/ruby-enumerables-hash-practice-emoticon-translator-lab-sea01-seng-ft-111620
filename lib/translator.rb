63b9a047a20b475aa9371004b7269e4d37772be6
Charlie Knight <williamchknight@gmail.com>
2020-11-09 04:50:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,28 +11,28 @@ def load_library(path)
   end
 end
 
-def get_japanese_emoticon(path, emoticon)
-  emoticon_hash = load_library(path)
-  empty = []
-  emoticon_hash.each do |word, language|
-    language.each do |lang, emoticon|
-     empty << emoticon       #add all the emoticons to a new array 
-    end
-  end
-  hash = Hash[*empty]        #build a hash english emoticon as key and japanese as value 
-  emote = hash[emoticon]      #call japanese emoticon with english emoticon
-  if emote == nil
-    emote = "Sorry, that emoticon was not found" 
-  end
-emote 
-end
+# def get_japanese_emoticon(path, emoticon)
+#   emoticon_hash = load_library(path)
+#   empty = []
+#   emoticon_hash.each do |word, language|
+#     language.each do |lang, emoticon|
+#     empty << emoticon       #add all the emoticons to a new array 
+#     end
+#   end
+#   hash = Hash[*empty]        #build a hash english emoticon as key and japanese as value 
+#   emote = hash[emoticon]      #call japanese emoticon with english emoticon
+#   if emote == nil
+#     emote = "Sorry, that emoticon was not found" 
+#   end
+# emote 
+# end
 
 
-def get_english_meaning(path, emoticon)
-  emoticon_hash = load_library(path)
-  invert = emotion_hash.invert 
-  binding.pry 
-end 
+# def get_english_meaning(path, emoticon)
+#   emoticon_hash = load_library(path)
+#   invert = emotion_hash.invert 
+#   binding.pry 
+# end 
 
 
 
