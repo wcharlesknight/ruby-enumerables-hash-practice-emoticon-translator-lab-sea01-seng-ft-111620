53b35a2dfa0af81ea5627c582fba82fd2b5d0860
Charlie Knight <williamchknight@gmail.com>
2020-11-09 04:45:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,11 +16,11 @@ def get_japanese_emoticon(path, emoticon)
   empty = []
   emoticon_hash.each do |word, language|
     language.each do |lang, emoticon|
-     empty << emoticon 
+     empty << emoticon       #add all the emoticons to a new array 
     end
   end
-  hash = Hash[*empty]
-  emote = hash[emoticon]
+  hash = Hash[*empty]        #build a hash english emoticon as key and japanese as value 
+  emote = hash[emoticon]      #call japanese emoticon with english emoticon
   if emote == nil
     emote = "Sorry, that emoticon was not found" 
   end
@@ -29,6 +29,8 @@ end
 
 
 def get_english_meaning(path, emoticon)
+  emoticon_hash = load_library(path)
+  binding.pry 
 end 
 
 
