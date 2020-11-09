7f9ae57aeb5ebcabb1f2e75fa71358e31a2506ba
Charlie Knight <williamchknight@gmail.com>
2020-11-09 03:45:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,12 +13,13 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
+  empty = {}
   emoticon_hash.each do |word, symbols|
    symbols.each do |k, v|
-    empty << v
-    trial << k 
+     
    end
   end
+  binding.pry
 end
 
 def get_english_meaning(path, emoticon)
